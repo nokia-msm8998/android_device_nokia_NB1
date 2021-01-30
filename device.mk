832755ea31eb7dd18c00f6bb32a4447c70a66e1d
Lunarixus <Nathan@lunarixus.dev>
2021-01-30 19:58:14 +0000

    NB1: Build netutils wrapper
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

--- device.mk
+++ device.mk
@@ -470,6 +470,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-qcom \
     TetheringConfigOverlay \
+    netutils-wrapper-1.0 \
     wcnss_service \
     WifiOverlay \
     wpa_supplicant \
