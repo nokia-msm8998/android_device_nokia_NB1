723f2b5193addfa7f641eb93c4139e3325fafb8c
log1cs <tuan73176@gmail.com>
2023-03-06 23:39:41 +0700

    NB1: LineageOS bringup

--- lineage_NB1.mk
+++ lineage_NB1.mk
@@ -9,19 +9,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common Lineage stuff
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from NB1 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-## Boot Animation
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_SCREEN_HEIGHT := 2560
-TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_NAME := lineage_NB1
 PRODUCT_DEVICE := NB1
