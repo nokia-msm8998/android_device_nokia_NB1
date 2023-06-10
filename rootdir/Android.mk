97f367beb7f21aaa7afe3609a108b58d2ee4678f
Tuan Anh <2251283500@e.tlu.edu.vn>
2023-06-10 11:42:59 +0700

    NLA: Goodbye, NB1!
    * We need to repartition, and therefore are unable to support
      upgrades from previous versions, hence, rebrand.
    
    * Also cleanup the tree and remove the offending entries.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,10 +1,10 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.nb1.smartamp_init.sh
+LOCAL_MODULE       := init.NLA.smartamp_init.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := bin/init.nb1.smartamp_init.sh
+LOCAL_SRC_FILES    := bin/init.NLA.smartamp_init.sh
 LOCAL_VENDOR_MODULE    := true
 include $(BUILD_PREBUILT)
 
@@ -46,22 +46,6 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES    := bin/init.qcom.usb.sh
 LOCAL_VENDOR_MODULE    := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.nb1.camera.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.nb1.camera.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.nb1.target.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.nb1.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
