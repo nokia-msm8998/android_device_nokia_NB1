92a1998ca00f7209592a66b9b59b7018eb46ea29
me-cafebabe <me_cafebabe@hotmail.com>
2023-07-30 15:48:38 +0000

    NLA: Setup Retrofit Dynamic Partitions

--- device.mk
+++ device.mk
@@ -122,7 +122,7 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     mm-qcamera-app \
     libhal_dbg
-
+    
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.CDS=off
 
@@ -180,6 +180,15 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4 \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
+    
+# Dynamic Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
 
 # Fingerprint
 PRODUCT_PACKAGES += \
