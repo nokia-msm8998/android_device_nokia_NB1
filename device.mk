2ee3cc35fda462e440e6e993606786f6937e08e5
Tuan Anh <tuan73176@gmail.com>
2023-11-04 17:57:47 +0700

    NLA: Provide android.hardware.bluetooth system variant
    Change-Id: Ia84cf4b22450b4afcfade3968e585fd2ff9367cf

--- device.mk
+++ device.mk
@@ -70,6 +70,8 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0.vendor \
+    android.hardware.bluetooth@1.0 \
+    android.hardware.bluetooth@1.1 \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
