12a9a4c929d2ae9a38e3f052c81426b32fc72920
Tuan Anh <tuan73176@gmail.com>
2023-11-20 19:45:30 +0700

    NLA: Use patchelf 0.17.2 for fingerprint blobs only

--- device.mk
+++ device.mk
@@ -195,7 +195,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.stats@1.0.vendor \
     android.hardware.biometrics.fingerprint@2.1.vendor \
     android.hardware.biometrics.fingerprint@2.2.vendor \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    libhidlbase_shim
 
 # FM
 PRODUCT_PACKAGES += \
