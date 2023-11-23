018ed1dbedd0307294766a07d3f508716202f453
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-11-23 21:40:05 +0700

    NLA: Keep building health HIDL interface lib in vendor
    * GNSS HAL needs it
    
    Change-Id: Id2e1b2844d195d41fc67a0224e0dc39573f7702e

--- device.mk
+++ device.mk
@@ -212,7 +212,8 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
-    android.hardware.health-service.qti_recovery
+    android.hardware.health-service.qti_recovery \
+    android.hardware.health@2.1.vendor
 
 # HIDL
 PRODUCT_PACKAGES += \
