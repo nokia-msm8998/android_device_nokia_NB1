c372a60d06a28fd417dac051354096dcd870f7b5
Tuan Anh <tuan73176@gmail.com>
2024-02-06 17:40:34 +0700

    NLA: Init empty device makefiles
    Change-Id: I1c806e3d4dcc0307fcfb19d35fdfbaaec7e22716

---
+++ device.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/nokia/NLA
+
+# Inherit device specific vendor makefiles
+$(call inherit-product, vendor/nokia/NLA/NLA-vendor.mk)
