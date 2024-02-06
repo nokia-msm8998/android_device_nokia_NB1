bdfcca94c91649b7075b03ebc5ebcef20662b479
Tuan Anh <tuan73176@gmail.com>
2024-02-06 17:40:34 +0700

    NLA: Init empty device makefiles
    Change-Id: I1c806e3d4dcc0307fcfb19d35fdfbaaec7e22716

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/nokia/NLA
+
 # Inherit from common device tree
 include device/nokia/msm8998-common/BoardConfigCommon.mk
 
---
+++ device.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/nokia/NLA
+
+# Inherit from the common device configuration.
+$(call inherit-product, device/nokia/msm8998-common/msm8998-common.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
+
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
+# Inherit device specific vendor makefiles
+$(call inherit-product, vendor/nokia/NLA/NLA-vendor.mk)
