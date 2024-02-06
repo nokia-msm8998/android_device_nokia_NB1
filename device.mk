0232ee7e67ebec514cf9237dd3c06d8db0957149
Tuan Anh <tuan73176@gmail.com>
2024-02-06 17:40:34 +0700

    NB1: Init empty device makefiles
    Change-Id: I1c806e3d4dcc0307fcfb19d35fdfbaaec7e22716

---
+++ device.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/nokia/NB1
+
+# Inherit from the common device configuration.
+$(call inherit-product, device/nokia/msm8998-common/msm8998-common.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
+
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
+# Inherit device specific vendor makefiles
+$(call inherit-product, vendor/nokia/NB1/NB1-vendor.mk)
