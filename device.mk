a31597544395a20eadc0a34ee6f2f52b872bf3ec
George <geoster3d@gmail.com>
2024-02-17 10:25:08 +0700

    NLA: Build light HAL from source
    Change-Id: Id8da37bf1df527eabb880dc8f0b6567173e047a5

--- device.mk
+++ device.mk
@@ -10,6 +10,12 @@ DEVICE_PATH := device/nokia/NLA
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/goodix_fp.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix_fp.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    lights.msm8998
+
 # Shims
 PRODUCT_PACKAGES += \
     libshim_binder
