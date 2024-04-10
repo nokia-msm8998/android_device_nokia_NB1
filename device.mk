ae9a29c79704f5691a473e8e102b45b084e19e61
Tuan Anh <tuan73176@gmail.com>
2024-04-10 10:15:35 +0700

    NB1: Set up platform info and fstab
    * Specify board platform while at it, set correct fstab.
    * fstab taken from common tree, removed vendor partition
      because NB1 doesn't have it.
    
    Change-Id: I7450576ea97cf129da71587838593172a6f0625f

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.nb1.camera.rc \
-    init.nb1.target.rc
+    init.nb1.target.rc \
+    fstab.qcom
 
 # Inherit device specific vendor makefiles
 $(call inherit-product, vendor/nokia/NB1/NB1-vendor.mk)
