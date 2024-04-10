6e7f0f712fb0d2d02a9e8ddd517488fe1392774b
Tuan Anh <tuan73176@gmail.com>
2024-04-10 10:15:35 +0700

    NLA: Set up platform info and fstab
    * Specify board platform while at it, set correct fstab.
    * fstab taken from common tree, removed vendor partition
      because NB1 doesn't have it.
    
    Change-Id: I7450576ea97cf129da71587838593172a6f0625f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/nokia/NLA
 # Inherit from common device tree
 include device/nokia/msm8998-common/BoardConfigCommon.mk
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msm8998
+
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_NLA_defconfig
 
@@ -18,6 +21,15 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 # Props
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3728096384
+
+# QCOM
+TARGET_BOARD_PLATFORM := msm8998
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Include the proprietary files
 include vendor/nokia/NLA/BoardConfigVendor.mk
