c5789061ea6c55a2548c58131a1e4f117591f91c
Roshan Pius <rpius@google.com>
2024-05-05 12:38:15 +0700

    NLA: Add WifiOverlayNB1
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- device.mk
+++ device.mk
@@ -38,5 +38,9 @@ PRODUCT_PACKAGES += \
     init.nb1.target.rc \
     fstab.qcom
 
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    WifiOverlayNB1
+
 # Inherit device specific vendor makefiles
 $(call inherit-product, vendor/nokia/NLA/NLA-vendor.mk)
