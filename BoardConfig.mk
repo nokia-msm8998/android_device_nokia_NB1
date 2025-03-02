161edf783aee17d4a75774b9b6ab8ed85390879d
me-cafebabe <me_cafebabe@hotmail.com>
2025-03-02 08:35:49 +0700

    NB1: Specify a non-existent dt directory to avoid using dt fstab
    Change-Id: I5b4df1f1a3e05b1d19e6770bef91ba4bc65435e6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@ AB_OTA_PARTITIONS += \
     odm
 
 # Kernel
-BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1da4000.ufshc
+BOARD_KERNEL_CMDLINE += androidboot.android_dt_dir=/non-existent androidboot.boot_devices=soc/1da4000.ufshc
 
 # QCOM
 TARGET_BOARD_PLATFORM := msm8998
